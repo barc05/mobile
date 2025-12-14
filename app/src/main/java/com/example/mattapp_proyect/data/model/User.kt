2a75c86332aa4c1f2ab7986a26b00a134ac26478
Cri008d <cri.zunigam@duocuc.cl>
2025-12-14 20:30:48 -0300

    Add serialization support to User data class

--- app/src/main/java/com/example/mattapp_proyect/data/model/User.kt
+++ app/src/main/java/com/example/mattapp_proyect/data/model/User.kt
@@ -1,6 +1,9 @@
 package com.example.mattapp_proyect.data.model
 
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerialName
 
+@Serializable
 data class User (
     val id: String? = null,
     val nombre: String,
