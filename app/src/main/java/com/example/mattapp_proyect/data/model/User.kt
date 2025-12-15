68907617701719e16c579ea941a2e95b8973e370
Cri008d <cri.zunigam@duocuc.cl>
2025-12-14 21:58:54 -0300

    Update User.kt

--- app/src/main/java/com/example/mattapp_proyect/data/model/User.kt
+++ app/src/main/java/com/example/mattapp_proyect/data/model/User.kt
@@ -8,7 +8,6 @@ data class User (
     val id: String? = null,
     val nombre: String,
     val correo: String,
-    val contraseña: String,
     val rol: String,
     val fotoUri: String? = null
 )
